10ecadf6ff4f135703f9f6de35cbddc7cc696400
Ymow Wu <kenshin0812@hotmail.com>
2016-03-17 21:31:47 +0800

    webhook

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ class AwesomeProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native, Ymow!
+          Welcome to React Native, Ymow! github webhook
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
